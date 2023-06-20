2e16eb686d84841f36344824c680c44ef240717f
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-06-20 16:06:46 -0500

    ChristmasWithTypesExercise

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace ChristmasWithTypes
 {
@@ -11,16 +11,16 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = Kris Kringle;
+            xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{guitar, violin, bass }; 
+            xmas.Presents =  new string[3]{"guitar", "violin", "bass" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
 
-            Console.WriteLine($"This year christmas falls on {Thursday} \n");
-            Console.WriteLine($"Our tree will be {10.TreeHeight} feet high \n");
+            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
+            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
             Console.WriteLine("Here are the presents we would like:");
 
             foreach (var present in xmas.Presents)
